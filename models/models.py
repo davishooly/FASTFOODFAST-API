4e98083b4206923ebe2d01aa8007a55ff72e5ed1
davishooly <kimamedavis87@gmail.com>
2018-09-04 19:44:31 +0300

    [Feature #160246815] Add order model to models.py file

--- models/models.py
+++ models/models.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+FoodItems = []
+FoodOrders = []
+
 
 class FoodItem:
 
@@ -17,12 +20,38 @@ class FoodItem:
     def serialize(self):
         """ serialize a FoodItem object to a dictionary"""
         return dict(
-            _id=self.id,
+            id=self.id,
             name=self.name,
             description=self.description,
             date=str(self.date),
             price=self.price,
         )
 
+    def get_by_id(self, _id):
+        for food_item in FoodItems:
+            if food_item.id == _id:
+                return food_item
 
-FoodItems = []
+
+class FoodOrder:
+
+    food_order_id = 1
+
+    def __init__(self, name=None,  destinantion=None):
+        self.destinantion = destinantion
+        self.name = name
+        self.date = datetime.now().replace(second=0, microsecond=0)
+        self.status = "PENDING"
+        self._id = FoodOrder.food_order_id
+
+        FoodOrder.food_order_id += 1
+
+    def serialize(self):
+        """ serialize a Food Order object to a dictionary"""
+        return dict(
+            id=self._id,
+            name=self.name,
+            status=self.status,
+            destinantion=self.destinantion,
+            date=str(self.date),
+        )
