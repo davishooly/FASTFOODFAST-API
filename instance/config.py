186b2c8d2ee7a100c08239453b0fcc8e620c2dfc
davishooly <kimamedavis87@gmail.com>
2018-09-04 17:55:29 +0300

    [Feature #160249398] Add config settings to  config.py file

---
+++ instance/config.py
@@ -0,0 +1,29 @@
+class Config:
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
