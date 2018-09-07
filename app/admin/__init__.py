5b49cf7d50dc6ae8542912c45161490a181a054e
davishooly <kimamedavis87@gmail.com>
2018-09-07 13:51:53 +0300

    [Feature #160342618] Add blueprint for Specific food item

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
-from .admin_views import Foods, SpecificOrder
+from .admin_views import Foods, SpecificOrder, SpecificFoodItem
 
 admin_blueprint = Blueprint("admin", __name__)
