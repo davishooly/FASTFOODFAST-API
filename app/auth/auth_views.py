9b42a32f6c52f52f77bf9f604aad01afa0346198
davishooly <hoolykim@gmail.com>
2018-09-28 01:59:43 +0300

    [FIX ] Fix method to fetch by id from database

--- app/auth/auth_views.py
+++ app/auth/auth_views.py
@@ -1,17 +1,18 @@
+# module imports
 from flask import Flask
 
-
 from flask_restful import Resource, reqparse
-
-from models.models import User, Users
-
-from utils import validators
 
 from werkzeug.security import check_password_hash
 
 from flask_jwt_extended import create_access_token
 
 import datetime
+
+# local imports
+from models.models import User
+
+from utils import validators
 
 
 class SignUp(Resource):
@@ -24,8 +25,8 @@ class SignUp(Resource):
                         help="This field can not be left bank")
     parser.add_argument("password", type=str, required=True,
                         help="This field can not be left bank")
-    parser.add_argument("is_admin", type=int, required=True,
-                        help="This field can not be left bank")
+    parser.add_argument('is_admin', type=bool, required=True,
+                        help='This field cannot be left blank')
 
     def post(self):
         """ Create a new user"""
@@ -49,20 +50,17 @@ class SignUp(Resource):
             return {"message": "password should start with a capital letter"
                     " and include a number"}, 400
 
-        if is_admin not in range(0, 2):
-            return {"message": " must be one or zero"}, 400
+        if User().fetch_by_username(username):
+            return {"message": "user {} already exists".format(username)}, 400
 
-        if User().get_by_username(username):
-            return {"message": f"user {username} already exists"}, 400
+        if User().fetch_by_email(email):
+            return {"message": "user with {} already exists".format(email)}, 400
 
-        if User().get_by_email(email):
-            return {"message": f"user with {email} already exists"}, 400
+        user = User(username, email, password, is_admin)
 
-        user = User(username, email, password, bool(is_admin))
+        user.add()
 
-        Users.append(user)
-
-        return {"message": f"user {username} created successfully"}, 201
+        return {"message": "user {} created successfully".format(username)}, 201
 
 
 class Login(Resource):
@@ -89,10 +87,14 @@ class Login(Resource):
             return {"message": "password should start with a capital letter"
                     " and include a number"}, 400
 
-        user = User().get_by_username(username)
+        user = User().fetch_by_username(username)
 
-        if user and check_password_hash(user.password_hash, password):
-            expires = datetime.timedelta(minutes=30)
-            token = create_access_token(user.username, expires_delta=expires)
-            return {'token': token, 'message': 'successfully logged'}, 200
-        return {'message': 'user not found'}, 404
+        if not user:
+            return {'message': 'user not found'}, 404
+
+        if not check_password_hash(user.password_hash, password):
+            return {'message': 'incorrect password'}
+
+        expires = datetime.timedelta(minutes=30)
+        token = create_access_token(user.username, expires_delta=expires)
+        return {'token': token, 'message': 'successfully logged'}, 200
