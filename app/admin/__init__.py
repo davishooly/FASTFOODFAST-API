7108b8db684bfc7b6609d5de8c7ef6f5ee04c7e6
davishooly <hoolykim@gmail.com>
2018-09-28 01:34:11 +0300

    [Feature] Add admin operations to blueprints in __init__.py file

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
-from .admin_views import Foods, SpecificOrder, SpecificFoodItem
+from .admin_views import Foods, SpecificOrder, SpecificFoodItem, AcceptFoodOrders, AcceptedOrders, RejectFoodOrders, CompletedOrders, CompleteFoodOrders, GetOrders
+
 
 admin_blueprint = Blueprint("admin", __name__)
